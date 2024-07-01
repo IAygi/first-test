9d7fb97be0ef1042557bf910a4dfa075073faf5c
Innokentiy Aygi <iaygi@fil-it.ru>
2024-07-01 14:06:35 +0300

    first commit

---
+++ src/test/java/ru/iaygi/FirstTest.java
@@ -0,0 +1,13 @@
+package ru.iaygi;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FirstTest {
+
+    @Test
+    void uiTest() {
+        assertEquals(1, 1);
+    }
+}
