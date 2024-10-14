6be1d09ebed93fe71a2a43e7fa319d80b32d1d9e
kolomiets.aa <kolomiets.aan@gazprom-neft.ru>
2024-10-14 12:57:34 +0300

    Таска 1572

--- src/test/java/ru/iaygi/api/artem/ApiTest.java
+++ src/test/java/ru/iaygi/api/artem/ApiTest.java
@@ -8,48 +8,33 @@ import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.hasItems;
+
 
 public class ApiTest {
 
-    private final static String BASE_URL = "https://reqres.in";
+    private final static String BASE_URL = "https://reqres.in/api";
+
 
     @Test
     @Tag("smoke")
-    @DisplayName("Получение пользователя")
-    @Description("Проверить корректное получение пользователя")
-    void getUser() {
+    @DisplayName("Проверка роута unknown")
+    @Description("Проверить значения параметров в ответе")
+    void getUnknownTest() {
         given().
                 contentType(ContentType.JSON).
+                pathParam("id", 2).
+                pathParam("route", "unknown").
                 when().
                 log().all().
-                get(BASE_URL + "/api/users/2").
+                get(BASE_URL + "/{route}/{id}").
                 then().
                 log().all().
                 assertThat().statusCode(200).
                 body("data.id", equalTo(2)).
-                body("data.email", equalTo("janet.weaver@reqres.in")).
-                body("data.first_name", equalTo("Janet")).
-                body("data.last_name", equalTo("Weaver"));
-    }
+                body("data.name", equalTo("fuchsia rose")).
+                body("data.year", equalTo(2001)).
+                body("data.color", equalTo("#C74375")).
+                body("data.pantone_value", equalTo("17-2031"));
 
-    @Test
-    @Tag("smoke")
-    @DisplayName("Получение списка пользователей")
-    @Description("Проверить корректное получение списка пользователей")
-    void getUsers() {
-        given().
-                contentType(ContentType.JSON).
-                queryParam("page", 2).
-                when().
-                log().all().
-                get(BASE_URL + "/api/users").
-                then().
-                log().all().
-                assertThat().statusCode(200).
-                body("data.id", hasItems(7, 8, 9, 10, 11, 12)).
-                body("data.email", hasItems("michael.lawson@reqres.in", "lindsay.ferguson@reqres.in", "tobias.funke@reqres.in", "byron.fields@reqres.in", "george.edwards@reqres.in", "rachel.howell@reqres.in")).
-                body("data.first_name", hasItems("Michael", "Lindsay", "Tobias", "Byron", "George", "Rachel")).
-                body("data.last_name", hasItems("Lawson", "Ferguson", "Funke", "Fields", "Edwards", "Howell"));
     }
 }
